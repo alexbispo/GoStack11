8fb2a60971fe2435c34130e8df6abd3d720ea0d0
Alex Bispo <alexbispo.artes@gmail.com>
2020-04-16 06:55:41 +0000

    Implements list appointments route. Refactor to use DTOs.

--- week02/classes/backend/src/routes/appointments.routes.ts
+++ week02/classes/backend/src/routes/appointments.routes.ts
@@ -6,6 +6,12 @@ const appointmentsRouter = Router();
 
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (req, res) => {
+  const allAppointments = appointmentsRepository.all();
+
+  return res.json(allAppointments);
+});
+
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
@@ -21,7 +27,10 @@ appointmentsRouter.post('/', (req, res) => {
       .json({ message: 'This appointment is already booked.' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return res.json(appointment);
 });
