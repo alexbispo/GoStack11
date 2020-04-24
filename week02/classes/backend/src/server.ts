0472758e5bdbb937dcf9e1026ce9652358ec4c6a
Alex Bispo <alexbispo.artes@gmail.com>
2020-04-24 08:07:11 +0000

    Implements a route to serve static files

--- week02/classes/backend/src/server.ts
+++ week02/classes/backend/src/server.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata';
-
 import express from 'express';
-import routes from './routes';
-
 import './database';
+import routes from './routes';
+import configUpload from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/files', express.static(configUpload.dir));
 
 app.use(routes);
 
